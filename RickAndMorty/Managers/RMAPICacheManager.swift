556f90bbc3531379bab4a4544a013437d03ceb21
luksinocencio <luksinocencio@gmail.com>
2023-09-13 01:03:30 -0300

    feat: add managers

---
+++ RickAndMorty/Managers/RMAPICacheManager.swift
@@ -0,0 +1,8 @@
+//
+//  RMAPICacheManager.swift
+//  RickAndMorty
+//
+//  Created by Lucas Inocencio on 13/09/23.
+//
+
+import Foundation
---
+++ RickAndMorty/Managers/RMImageLoader.swift
@@ -0,0 +1,8 @@
+//
+//  RMImageLoader.swift
+//  RickAndMorty
+//
+//  Created by Lucas Inocencio on 13/09/23.
+//
+
+import Foundation
