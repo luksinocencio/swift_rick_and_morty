c77f6a24d13280516ebaa56a2ec8f637a1186ab1
Lucas Inocêncio <luksinocencio@gmail.com>
2024-09-20 12:48:27 -0300

    feat: add SwiftUI Settings View

--- RickAndMorty/Controllers/Core/RMSettingsViewController.swift
+++ RickAndMorty/Controllers/Core/RMSettingsViewController.swift
@@ -1,19 +1,42 @@
 import UIKit
+import SwiftUI
 
 /// Controller to show various app options and settings
 final class RMSettingsViewController: UIViewController {
     
     // MARK: - Private property(ies).
-    private let viewModel = RMSettingsViewViewModel(
-        cellViewModels: RMSettingsOption.allCases.compactMap({
-            return RMSettingsCellViewModel(type: $0)
-        })
+    private let settingsSwiftUIController = UIHostingController(
+        rootView: RMSettingsView(
+            viewModel: RMSettingsViewViewModel(
+                cellViewModels: RMSettingsOption.allCases.compactMap({
+                    return RMSettingsCellViewModel(type: $0)
+                })
+            )
+        )
     )
     
+    // MARK: - Lifecycle(s).
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Settings"
         view.backgroundColor = .systemBackground
+        addSwiftUIController()
+    }
+    
+    // MARK: - Private function(s).
+    private func addSwiftUIController() {
+        addChild(settingsSwiftUIController)
+        settingsSwiftUIController.didMove(toParent: self)
+        
+        view.addSubview(settingsSwiftUIController.view)
+        settingsSwiftUIController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            settingsSwiftUIController.view.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            settingsSwiftUIController.view.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            settingsSwiftUIController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            settingsSwiftUIController.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
