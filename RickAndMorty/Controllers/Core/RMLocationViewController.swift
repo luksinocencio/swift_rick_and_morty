7f0fb01020e5e4f384b3df5fd742d9b14c1c427b
Lucas Inocêncio <luksinocencio@gmail.com>
2024-10-05 02:37:39 -0300

    feat: begin location tab

--- RickAndMorty/Controllers/Core/RMLocationViewController.swift
+++ RickAndMorty/Controllers/Core/RMLocationViewController.swift
@@ -2,22 +2,46 @@ import UIKit
 
 /// Controller to show and search for Location
 final class RMLocationViewController: UIViewController {
+    private let primaryView = RMLocationView()
+    private let viewModel = RMLocationViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "Locations"
-        view.backgroundColor = .systemBackground
-        setupNavigationItemRightButton()
+        setupView()
     }
     
-    private func setupNavigationItemRightButton() {
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(didTapShare))
+    private func setupView() {
+        title = "Locations"
+        view.backgroundColor = .systemBackground
+        
+        addSearchButton()
+        addConstraints()
+    }
+    
+    private func addSearchButton() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .search,
+            target: self,
+            action: #selector(didTapShare)
+        )
+    }
+    
+    private func addConstraints() {
+        [primaryView].forEach(view.addSubview)
+        
+        NSLayoutConstraint.activate([
+            primaryView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            primaryView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            primaryView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            primaryView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
     
     // MARK: - Selector(s).
     
     @objc
     private func didTapShare() {
-        
+        print("click header button")
     }
 }
