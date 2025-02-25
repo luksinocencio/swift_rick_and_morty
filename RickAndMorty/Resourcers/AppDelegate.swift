d8870fe096da732c74c80ac003116b6ed678289f
luksinocencio <luksinocencio@gmail.com>
2025-02-25 10:35:55 -0300

    feat: add debug RocketSim

--- RickAndMorty/Resourcers/AppDelegate.swift
+++ RickAndMorty/Resourcers/AppDelegate.swift
@@ -3,7 +3,18 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        loadRocketSimConnect()
         return true
+    }
+    
+    private func loadRocketSimConnect() {
+        #if DEBUG
+        guard (Bundle(path: "/Applications/RocketSim.app/Contents/Frameworks/RocketSimConnectLinker.nocache.framework")?.load() == true) else {
+            print("Failed to load linker framework")
+            return
+        }
+        print("RocketSim Connect successfully linked")
+        #endif
     }
 
     // MARK: UISceneSession Lifecycle
